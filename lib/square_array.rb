f6004bb83bc55e3123b37dc274e0a18c1c8c03e4
Anthony Oglesby <ogle1ant@gmail.com>
2019-08-08 16:47:11 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,4 +1,6 @@
+counter = 0
 def square_array(array)
+<<<<<<< HEAD
   new_array = []
   
   counter = 0
@@ -7,4 +9,11 @@ def square_array(array)
     counter += 1
   end
   return new_array
+=======
+    
+  while array[counter] < array.length do
+    array = array[counter]**array[counter]
+    counter += 1
+  end
+>>>>>>> 4b2d62f679545d3ccc6d13eeb25da9ff572f07c6
 end
